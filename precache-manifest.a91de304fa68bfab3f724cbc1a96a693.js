7293ee2223a19af41455e35e3d0635556246e060
louisedly <krizia.deloyola1@gmail.com>
2020-08-12 17:50:23 -0400

    Updates

---
+++ precache-manifest.a91de304fa68bfab3f724cbc1a96a693.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "917d3485bc7ad1fdf79ef6e6d4ee781c",
+    "url": "/book-search-app/index.html"
+  },
+  {
+    "revision": "9835c8e5b3a41d369828",
+    "url": "/book-search-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "06208b17d78b9558e497",
+    "url": "/book-search-app/static/js/2.a05ad7f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/book-search-app/static/js/2.a05ad7f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9835c8e5b3a41d369828",
+    "url": "/book-search-app/static/js/main.c7bf5f08.chunk.js"
+  },
+  {
+    "revision": "d9ebb8659e0728ff2783",
+    "url": "/book-search-app/static/js/runtime-main.9fccfaa1.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/book-search-app/precache-manifest.a91de304fa68bfab3f724cbc1a96a693.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/book-search-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
